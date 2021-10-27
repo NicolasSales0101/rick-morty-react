1652f44e0fe56fba94d0f8c6aebdf7d8b662511d
NicolasSales0101 <nicolascavalcante0101@gmail.com>
2021-10-27 16:41:47 -0400

    Chamando o componente principal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import HeaderResponsive from './Components/HeaderResponsive';
+import MainSection from './Components/MainSection';
 
 function App() {
   return (
     <div className="App">
-      <header className="header">
-        <HeaderResponsive />
-      </header>
+      <HeaderResponsive />  
+      <MainSection />
     </div>
   );
 }
